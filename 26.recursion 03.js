475dcd5fa5d3c686e916f637de44507d7c72eb9a
Arafat Sarkar <arafatgovt01@gmail.com>
2024-02-18 16:17:51 +0600

    minor ; corrcection

--- 26.recursion 03.js
+++ 26.recursion 03.js
@@ -18,7 +18,7 @@ function backwardPrint(arr,length){
 
 var array01=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 forwardPrint(array01,array01.length);
-console.log("======================================================")
+console.log("======================================================");
 backwardPrint(array01,array01.length);
 
 //Think yourself ?!?!?!!
